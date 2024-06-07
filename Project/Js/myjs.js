8f0e36ffa9a4ce35a6ac2f073ef5e11058c4bc51
Harsh Doshi <115213620+Harshdoshi1@users.noreply.github.com>
2024-06-07 14:15:00 +0530

    push

--- Project/Js/myjs.js
+++ Project/Js/myjs.js
@@ -1,9 +1,9 @@
 
-let name = "harsh"
+let firstName = "harsh";
 let age = 20;
 let student = true;
 
-console.log("hello muy name is",name);
+console.log("hello my name is",firstName);
 console.log("my age is",age);
 
-document.getElementById("p1").innerHTML = "hello" + name;
+document.getElementById("p1").innerHTML =  firstName;
