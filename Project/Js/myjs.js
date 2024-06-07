1822458c051565c3d80ade71db265dec98391751
Harsh Doshi <115213620+Harshdoshi1@users.noreply.github.com>
2024-06-07 14:29:54 +0530

    push

--- Project/Js/myjs.js
+++ Project/Js/myjs.js
@@ -6,4 +6,11 @@ let student = true;
 console.log("hello my name is",firstName);
 console.log("my age is",age);
 
-document.getElementById("p1").innerHTML =  firstName;
+document.getElementById("p1").innerHTML =  firstName + " hello";
+
+let username;
+
+document.getElementById("button").onclick = function(){
+    username = document.getElementById("mytext").value;
+    console.log(username);
+}
